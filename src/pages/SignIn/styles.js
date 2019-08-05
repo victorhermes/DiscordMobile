2d8e69bdc6948cb74abd60a5e25e0e56d6b4f12a
Victor <me.victorhermes@gmail.com>
2019-08-04 22:25:14 -0300

    styles

--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -2,23 +2,23 @@ import { StyleSheet } from 'react-native';
 import { colors, general } from '~/styles';
 
 const styles = StyleSheet.create({
-  ...general.formStyles,
-  ...general.buttonStyles,
+    ...general.formStyles,
+    ...general.buttonStyles,
 
-  container: {
-    flex: 1,
-    backgroundColor: colors.background,
-    justifyContent: 'center',
-    alignItems: 'stretch',
-    padding: 30,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: colors.background,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        padding: 30,
+    },
 
-  title: {
-    fontSize: 28,
-    marginBottom: 20,
-    color: colors.white,
-    textAlign: 'center',
-  },
+    title: {
+        fontSize: 28,
+        marginBottom: 20,
+        color: colors.white,
+        textAlign: 'center',
+    },
 });
 
 export default styles;
