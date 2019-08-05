7400e48cdb67fe03e97e8a94667e5f5e76bd0d1f
Victor <me.victorhermes@gmail.com>
2019-08-04 22:24:45 -0300

    styles

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -3,37 +3,37 @@ import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import { colors } from '~/styles';
 
 const styles = StyleSheet.create({
-  backgroundWrapper: {
-    flex: 1,
-    backgroundColor: colors.backgroundDarker,
-  },
+    backgroundWrapper: {
+        flex: 1,
+        backgroundColor: colors.backgroundDarker,
+    },
 
-  container: {
-    flex: 1,
-    backgroundColor: colors.background,
-    borderLeftWidth: 1,
-    borderRightWidth: 1,
-    borderColor: colors.darkTransparent,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: colors.background,
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        borderColor: colors.darkTransparent,
+    },
 
-  header: {
-    backgroundColor: colors.backgroundDarker,
-    borderBottomWidth: 1,
-    borderBottomColor: colors.darkTransparent,
-    paddingHorizontal: 20,
-    paddingTop: getStatusBarHeight() + 11,
-    height: 54 + getStatusBarHeight() + 10,
+    header: {
+        backgroundColor: colors.backgroundDarker,
+        borderBottomWidth: 1,
+        borderBottomColor: colors.darkTransparent,
+        paddingHorizontal: 20,
+        paddingTop: getStatusBarHeight() + 11,
+        height: 54 + getStatusBarHeight() + 10,
 
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
 
-  teamTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: colors.white,
-  },
+    teamTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: colors.white,
+    },
 });
 
 export default styles;
